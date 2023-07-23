173f0b5ddd4df4e8e487be3937e02dcf31d1e512
sanju156 <sanjushree464@gmail.com>
2023-07-23 19:25:01 +0200

    Add main files

--- main.py
+++ main.py
@@ -258,4 +258,163 @@ if foundTrajectories:
 else:
     print("No trajectories match the query.")
 
-    
+
+################ Closest-Pair-Distance
+import math
+# Simple Point class definition
+class Point:
+    def _init_(self, X, Y, timestamp):
+        self.X = X
+        self.Y = Y
+        self.timestamp = timestamp
+
+# Function to calculate the Euclidean distance between two points
+def euclidean_distance(point1, point2):
+    return math.sqrt((point1.X - point2.X) ** 2 + (point1.Y - point2.Y) ** 2)
+
+# Function to calculate the Closest-Pair-Distance for a trajectory
+def closest_pair_distance(trajectory):
+    num_points = len(trajectory)
+    min_distance = float('inf')
+
+    for i in range(num_points - 1):
+        for j in range(i + 1, num_points):
+            distance = euclidean_distance(trajectory[i], trajectory[j])
+            if distance < min_distance:
+                min_distance = distance
+
+    return min_distance
+
+# Example usage with multiple trajectories
+def calculate_closest_pair_distances(trajectories):
+    closest_distances = {}
+
+    for traj in trajectories:
+        closest_distance = closest_pair_distance(traj.points)
+        closest_distances[traj.number] = closest_distance
+
+    return closest_distances
+
+# Assuming listOfTrajectories is a list of 'region.Trajectory' objects
+closest_pair_distances = calculate_closest_pair_distances(listOfTrajectories)
+
+# Print the closest pair distances for each trajectory
+for traj_number, distance in closest_pair_distances.items():
+    print("Trajectory {}: Closest Pair Distance: {}".format(traj_number, distance))
+
+
+###################### Non-Zero Closest pair distance 
+import math
+
+# Simple Point class definition
+class Point:
+    def _init_(self, X, Y, timestamp):
+        self.X = X
+        self.Y = Y
+        self.timestamp = timestamp
+
+# Function to calculate the Euclidean distance between two points
+def euclidean_distance(point1, point2):
+    return math.sqrt((point1.X - point2.X) ** 2 + (point1.Y - point2.Y) ** 2)
+
+# Function to calculate the Closest-Pair-Distance for a trajectory
+def closest_pair_distance(trajectory):
+    num_points = len(trajectory)
+    min_distance = float('inf')
+
+    for i in range(num_points - 1):
+        for j in range(i + 1, num_points):
+            distance = euclidean_distance(trajectory[i], trajectory[j])
+            if distance < min_distance:
+                min_distance = distance
+
+    return min_distance
+
+# Example usage with multiple trajectories
+def calculate_closest_pair_distances(trajectories):
+    closest_distances = {}
+
+    for traj in trajectories:
+        closest_distance = closest_pair_distance(traj.points)
+        closest_distances[traj.number] = closest_distance
+
+    return closest_distances
+
+# Helper function to find a trajectory by its number
+def find_trajectory_by_number(trajectories, traj_number):
+    for traj in trajectories:
+        if traj.number == traj_number:
+            return traj
+    return None
+
+# Assuming listOfTrajectories is a list of 'region.Trajectory' objects
+closest_pair_distances = calculate_closest_pair_distances(listOfTrajectories)
+
+# Print the trajectories with non-zero closest pair distances and their data points
+for traj_number, distance in closest_pair_distances.items():
+    if distance > 0.0:
+        print("Trajectory {}: Closest Pair Distance: {}".format(traj_number, distance))
+        trajectory = find_trajectory_by_number(listOfTrajectories, traj_number)
+
+
+################# Dynamic Time Wrapping Distance
+import numpy as np
+from scipy.spatial.distance import euclidean
+from scipy.spatial.distance import cdist
+from scipy.spatial.distance import squareform
+
+# Simple Point class definition
+class Point:
+    def _init_(self, X, Y, timestamp):
+        self.X = X
+        self.Y = Y
+        self.timestamp = timestamp
+
+# Function to calculate the Dynamic Time Warping distance between two trajectories
+def dtw_distance(trajectory1, trajectory2):
+    trajectory1_points = np.array([(point.X, point.Y) for point in trajectory1])
+    trajectory2_points = np.array([(point.X, point.Y) for point in trajectory2])
+
+    # Calculate the pairwise Euclidean distances between all points in both trajectories
+    distance_matrix = cdist(trajectory1_points, trajectory2_points, euclidean)
+
+    # Initialize the cumulative cost matrix
+    cumulative_cost = np.zeros((len(trajectory1_points), len(trajectory2_points)))
+
+    # Fill the cumulative cost matrix
+    cumulative_cost[0, 0] = distance_matrix[0, 0]
+    for i in range(1, len(trajectory1_points)):
+        cumulative_cost[i, 0] = distance_matrix[i, 0] + cumulative_cost[i - 1, 0]
+
+    for j in range(1, len(trajectory2_points)):
+        cumulative_cost[0, j] = distance_matrix[0, j] + cumulative_cost[0, j - 1]
+
+    for i in range(1, len(trajectory1_points)):
+        for j in range(1, len(trajectory2_points)):
+            cumulative_cost[i, j] = distance_matrix[i, j] + min(
+                cumulative_cost[i - 1, j], cumulative_cost[i, j - 1], cumulative_cost[i - 1, j - 1]
+            )
+
+    # Return the DTW distance between the two trajectories
+    return cumulative_cost[-1, -1]
+
+# Function to calculate the DTW distance between all pairs of trajectories
+def calculate_dtw_distances(trajectories):
+    num_trajectories = len(trajectories)
+    dtw_distances = np.zeros((num_trajectories, num_trajectories))
+
+    for i in range(num_trajectories):
+        for j in range(i, num_trajectories):
+            dtw_dist = dtw_distance(trajectories[i].points, trajectories[j].points)
+            dtw_distances[i, j] = dtw_dist
+            dtw_distances[j, i] = dtw_dist
+
+    return dtw_distances
+
+# Assuming listOfTrajectories is a list of 'region.Trajectory' objects
+dtw_distances = calculate_dtw_distances(listOfTrajectories)
+
+# Print the DTW distances between all pairs of trajectories
+np.set_printoptions(threshold=np.inf)  # Set numpy display option to print full array
+print("Dynamic Time Warping Distances:")
+print(squareform(dtw_distances))      
